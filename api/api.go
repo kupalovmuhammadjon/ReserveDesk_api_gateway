f67d9fc8e4cc4799cc67ca0d9842f4c97879ad62
bobur <yusupovbobur1001@gmail.com>
2024-07-12 10:25:51 +0500

    .

--- api/api.go
+++ api/api.go
@@ -1,3 +1,42 @@
 package api
 
+import (
+	"api_service/api/handler"
+	"api_service/api/middleware"
+	"api_service/config"
 
+	"github.com/gin-gonic/gin"
+)
+
+func NewRouter(cfg *config.Config) *gin.Engine {
+	router := gin.Default()
+
+	api := router.Group("/reservation")
+	api.Use(middleware.JWTMiddleware())
+
+	h := handler.NewHandler(cfg)
+
+	// reservation api
+	reservation := api.Group("/reservations")
+	reservation.POST("/", h.CreateReservation)
+	reservation.DELETE("/:id", h.DeleteReservation)
+	reservation.PUT("/:id", h.UpdateReservation)
+	reservation.GET("/", h.GetAllReservation)
+	reservation.GET("/:id", h.GetByIdReservation)
+
+	// order api
+	order := api.Group("/orders")
+	order.POST("/", h.CreateOrder)
+	order.PUT("/:id", h.UpdateOrder)
+	order.DELETE("/:id", h.DeleteAuth)
+	order.GET("/", h.GetAllOrder)
+	order.GET("/:id", h.GetByIdOrder)
+
+	// auth api
+	auth := api.Group("/auths")
+	auth.PUT("/:id/profile", h.UpdateAuth)
+	auth.DELETE("/:id/profile", h.DeleteAuth)
+	auth.GET("/:id/profile", h.ShowProfile)
+
+	return router
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,14 @@
 package main
 
+import (
+	"api_service/api"
+	"api_service/config"
+)
 
 func main() {
-	
+	cfg := config.Config{}
+
+	router := api.NewRouter(&cfg)
+	router.Run(cfg.HTTP_PORT)
+
 }
